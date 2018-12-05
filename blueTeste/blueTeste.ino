30158b502edc07a87acb3b142145ef4e05d47e9b
FelipeCarisio <44804658+FelipeCarisio@users.noreply.github.com>
2018-12-04 23:07:17 -0200

    Add files via upload

---
+++ blueTeste/blueTeste.ino
@@ -0,0 +1,15 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial bt(2,3);
+
+void setup() 
+{
+  Serial.begin(9600);
+  bt.begin(9600);
+}
+
+void loop() {
+  bt.print("c");
+  bt.print("e");
+  delay(1000);
+}
---
+++ projetoMM/projetoMM.ino
@@ -0,0 +1,55 @@
+#include <Stepper.h>
+#include <Servo.h>
+
+int pinoSensor = A0;
+int valorSensor = 0;
+ 
+const int stepsPerRevolution = 200; 
+
+#define SERVO 7
+ 
+Servo s; 
+
+Stepper myStepper(stepsPerRevolution, 8, 10, 9, 11);
+
+int stepCount = 0;  // number of steps the motor has taken
+
+void setup() 
+{
+  s.attach(SERVO);
+  Serial.begin(9600);
+  s.write(120);
+}
+
+void loop() 
+{
+   myStepper.setSpeed(100);
+
+  myStepper.step(-620);
+  delay(1000); 
+
+  valorSensor = analogRead(pinoSensor);
+  valorSensor = map(valorSensor, 0, 1024, 0, 10);
+  delay(100);
+
+  Serial.println(valorSensor);
+  delay(1000);
+
+  if(valorSensor > 0 && valorSensor <= 5)
+  {
+    s.write(100);
+    delay(200);
+    myStepper.step(-180);
+    delay(100);
+  }
+  else 
+  {
+   s.write(170);
+   delay(200);
+   myStepper.step(-180); 
+   delay(100);
+  }
+   
+  myStepper.step(800);
+  delay(1000); 
+}
